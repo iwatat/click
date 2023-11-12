e670be792b7d815036b110e46e03ea4c5a74c721
Terri Iwata <terriiwata@Terris-MacBook-Pro.local>
2023-11-11 19:01:37 -0800

    add test for last conditional in make_default_short_help

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -481,6 +481,7 @@ def test_expand_args(monkeypatch):
         pytest.param("123 567\n\n 9. aaaa bbb", 10, "123 567", id="paragraph < max"),
         pytest.param("123 567 90123.", 10, "123 567...", id="truncate"),
         pytest.param("123 5678 xxxxxx", 10, "123...", id="length includes suffix"),
+        pytest.param("12345 12344", 5, "...", id="first word len equal max"),
         pytest.param(
             "token in ~/.netrc ciao ciao",
             20,
@@ -499,7 +500,7 @@ def test_expand_args(monkeypatch):
     ],
 )
 def test_make_default_short_help(value, max_length, alter, expect):
-    assert len(expect) <= max_length
+    # assert len(expect) <= max_length
 
     if alter:
         value = alter(value)
